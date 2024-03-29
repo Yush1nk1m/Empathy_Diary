b2712733289084dafd1dd8b9637716bf801b6ba3
Yush1nk1m <kys010306@sogang.ac.kr>
2024-03-30 05:58:51 +0900

    Feat: 감정 테이블 정의

---
+++ backend/models/emotion.js
@@ -0,0 +1,30 @@
+/**
+ * [감정 테이블]
+ * @typeName    감정의 종류
+ */
+const Sequelize = require("sequelize");
+
+class Emotion extends Sequelize.Model {
+    static initiate(sequelize) {
+        Emotion.initiate({
+            typeName: {
+                type: Sequelize.STRING(10),     // 최대 길이 10
+                allowNull: false,               // 값은 null일 수 없다.
+                unique: true,                   // 값은 유일해야 한다.
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: "Emotion",
+            tableName: "emotions",
+            paranoid: false,
+            charset: "utf8mb4",
+            collate: "utf8mb4_general_ci",
+        });
+    }
+
+    static associate(db) {}
+};
+
+module.exports = Emotion;
