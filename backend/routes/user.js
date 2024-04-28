845070231396fc4c431cba0ead6633852dc8e414
Yush1nk1m <kys010306@sogang.ac.kr>
2024-04-28 14:41:01 +0900

    Feat: 회원 라우터 코드 뼈대 생성

---
+++ backend/routes/user.js
@@ -0,0 +1,33 @@
+/*
+[User Router]
+
+회원과 관련된 정보들을 관리한다.
+
+| HTTP method | URI | API ID | role |
+| :--: | :-- | :--: | :-- |
+| GET | /users | u-01 | 회원 정보 조회 |
+| POST | /users | u-02 | 회원 가입 |
+| POST | /users/login | u-03 | 로그인 |
+| PATCH | /users | u-04 | 회원 정보 수정 |
+| DELETE | /users | u-05 | 회원 탈퇴 |
+*/
+const express = require("express");
+
+const router = express.Router();
+
+// [u-01] GET /users
+router.get("/users",);
+
+// [u-02] POST /users
+router.post("/users",);
+
+// [u-03] POST /users/login
+router.post("/users/login",);
+
+// [u-04] PATCH /users
+router.patch("/users",);
+
+// [u-05] DELETE /users
+router.delete("/users",);
+
+module.exports = router;
