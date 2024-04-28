cd3e9290d5f600886671fa3c4936ce3f6194bdf4
Yush1nk1m <kys010306@sogang.ac.kr>
2024-04-28 21:47:55 +0900

    Feat: 회원 가입, 로그인, 로그아웃 컨트롤러 구현

---
+++ backend/controllers/user.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const User = require("../models/user");
+
+// 회원 가입 컨트롤러
+exports.join = async (req, res, next) => {
+    const { userId, email, nickname, password } = req.body;
+
+    try {
+        const exUser = await User.findOne({ where: { userId } });
+        if (exUser) {
+            return res.status(409).send("이미 존재하는 회원 ID입니다.");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 50);
+        await User.create({
+            userId,
+            email,
+            nickname,
+            password: hashedPassword,
+        });
+
+        return res.status(200).send("회원 가입에 성공했습니다.");
+
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+}
+
+exports.login = (req, res, next) => {
+    passport.authenticate("local", (authError, user, info) => {
+        if (authError) {
+            return res.status(401).send("인증에 실패하였습니다.");
+        }
+
+        if (!user) {
+            return res.status(404).send("사용자 정보가 존재하지 않습니다.");
+        }
+
+        return req.login(user, (loginError) => {
+            if (loginError) {
+                console.error(loginError);
+                return next(loginError);
+            }
+
+            return res.status(200).send("로그인 성공");
+        })
+    })(req, res, next);     // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙인다.
+};
+
+exports.logout = (req, res) => {
+    req.logout(() => {
+        return res.status(200).send("로그아웃에 성공하였습니다.");
+    })
+};
