9d3abcb083127c4725d4907949569703bf6fdba7
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-06 21:26:06 +0900

    Test: post 모델 단위 테스트 작성

---
+++ backend/models/post.test.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const Post = require("./post");
+const config = require("../config/config")["test"];
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+);
+
+describe("Post model", () => {
+    test("static initiate 메소드를 호출한다.", () => {
+        expect(Post.initiate(sequelize)).toBe(undefined);
+    });
+
+    test("static associate 메소드를 호출한다.", () => {
+        const db = {
+            Post: {
+                belongsTo: jest.fn(),
+                belongsToMany: jest.fn(),
+                hasOne: jest.fn(),
+            },
+            User: {},
+            Emotion: {},
+            Sentiment: {},
+        };
+
+        Post.associate(db);
+
+        expect(db.Post.belongsTo).toBeCalledWith(db.User, { foreignKey: "writer", targetKey: "id", onDelete: "CASCADE", hooks: true });
+        expect(db.Post.belongsToMany).toBeCalledWith(db.Emotion, { through: "PostEmotions", onDelete: "CASCADE", hooks: true });
+        expect(db.Post.hasOne).toBeCalledWith(db.Sentiment, { foreignKey: "postId", sourceKey: "id" });
+    });
+});
