91a91b3dbd91bca6bee52617857d6357a87404da
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-06 21:32:13 +0900

    Test: advice 모델 단위 테스트 작성

---
+++ backend/models/advice.test.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const Advice = require("./advice");
+const config = require("../config/config")["test"];
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+);
+
+describe("Advice model", () => {
+    test("static initiate 메소드를 호출한다.", () => {
+        expect(Advice.initiate(sequelize)).toBe(undefined);
+    });
+
+    test("static associate 메소드를 호출한다.", () => {
+        const db = {
+            Advice: {
+                belongsTo: jest.fn(),
+                belongsToMany: jest.fn(),
+            },
+            User: {},
+            Emotion: {},
+        };
+
+        Advice.associate(db);
+
+        expect(db.Advice.belongsTo).toBeCalledWith(db.User, { foreignKey: "writer", targetKey: "id", onDelete: "CASCADE", hooks: true });
+        expect(db.Advice.belongsToMany).toBeCalledWith(db.Emotion, { through: "AdviceEmotions", onDelete: "CASCADE", hooks: true });
+    });
+});
