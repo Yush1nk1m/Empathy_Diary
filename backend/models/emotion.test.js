24bb3653563c75ae8f50f2bc3ba0cd4b34baf88b
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-06 21:36:16 +0900

    Test: emotion 모델 테스트 작성

---
+++ backend/models/emotion.test.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+const Emotion = require("./emotion");
+const config = require("../config/config")["test"];
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+);
+
+describe("Emotion model", () => {
+    test("static initiate 메소드를 호출한다.", () => {
+        expect(Emotion.initiate(sequelize)).toBe(undefined);
+    });
+
+    test("static associate 메소드를 호출한다.", () => {
+        const db = {
+            Emotion: {
+                belongsToMany: jest.fn(),
+            },
+            Post: {},
+            Advice: {},
+        };
+
+        Emotion.associate(db);
+
+        expect(db.Emotion.belongsToMany).toBeCalledWith(db.Post, { through: "PostEmotions", onDelete: "CASCADE", hooks: true });
+        expect(db.Emotion.belongsToMany).toBeCalledWith(db.Advice, { through: "AdviceEmotions", onDelete: "CASCADE", hooks: true });
+    });
+});
