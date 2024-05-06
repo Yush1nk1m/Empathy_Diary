6359ff883f5c87d7c1b0fb93fc97ce06ba1f37cc
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-06 21:14:01 +0900

    Test: user 모델 단위 테스트 작성

---
+++ backend/models/user.test.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const User = require("./user");
+const config = require("../config/config")["test"];
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+);
+
+describe("User model", () => {
+    test("static initiate 메소드를 호출한다.", () => {
+        expect(User.initiate(sequelize)).toBe(undefined);
+    });
+
+    test("static associate 메소드를 호출한다.", () => {
+        const db = {
+            User: {
+                hasMany: jest.fn(),
+            },
+            Post: {},
+            Advice: {},
+            Chatroom: {},
+        };
+
+        User.associate(db);
+
+        expect(db.User.hasMany).toBeCalledWith(db.Post, { foreignKey: "writer", sourceKey: "id", onDelete: "CASCADE", hooks: true });
+        expect(db.User.hasMany).toBeCalledWith(db.Advice, { foreignKey: "writer", sourceKey: "id", onDelete: "CASCADE", hooks: true });
+        expect(db.User.hasMany).toBeCalledWith(db.Chatroom, { foreignKey: "userId", sourceKey: "id", onDelete: "CASCADE", hooks: true });        
+    });
+});
