3fcaa033a1559182d81ecbd374efe431ba8138f2
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-08 12:42:54 +0900

    Test: [u-03] 통합 테스트 작성

--- backend/routes/user.test.js
+++ backend/routes/user.test.js
@@ -18,6 +18,13 @@ beforeAll(async () => {
             userId: "agent",
             password: "test",
     });
+    await request(app).post("/users").send({
+        userId: "user",
+        email: "user@sogang.ac.kr",
+        nickname: "테스트사용자",
+        password: "test",
+        confirmPassword: "test",
+    });
 });
 
 // [u-01] 회원 정보 조회
@@ -31,6 +38,16 @@ describe("[u-01] GET /users", () => {
     test("로그인되어 있으면 회원 정보 조회에 성공한다.", (done) => {
         agent
             .get("/users")
+            .expect(200, done);
+    });
+
+    test("유효한 요청을 보내면 로그인에 성공한다.", (done) => {
+        request(app)
+            .post("/users/login")
+            .send({
+                userId: "user",
+                password: "test",
+            })
             .expect(200, done);
     });
 });
@@ -87,6 +104,29 @@ describe("[u-02] POST /users", () => {
                 confirmPassword: "비밀번호",
             })
             .expect(200, done);
+    });
+});
+
+// [u-03] 로그인
+describe("[u-03] POST /users/login", () => {
+    test("회원 정보가 일치하지 않을 시 로그인에 실패한다.", (done) => {
+        request(app)
+            .post("/users/login")
+            .send({
+                userId: "agent",
+                password: ".",
+            })
+            .expect(400, done);
+    });
+
+    test("이미 로그인되어 있으면 로그인에 실패한다.", (done) => {
+        agent
+            .post("/users/login")
+            .send({
+                userId: "agent",
+                password: "test",
+            })
+            .expect(409, done);
     });
 });
 
