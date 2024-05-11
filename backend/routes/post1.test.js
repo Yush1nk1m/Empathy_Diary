9c81af99c79833ce68a4b3c21c40f6d67a0eb9f7
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-12 03:53:39 +0900

    Test: [p-02] 통합 테스트 작성

--- backend/routes/post1.test.js
+++ backend/routes/post1.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 const { sequelize } = require("../models");
 const { setEmotion } = require("../repositories");
-const { joinUserInfo, loginUserInfo } = require("../data/user");
+const { joinUserInfo, loginUserInfo, newJoinUserInfo, loginNewUserInfo } = require("../data/user");
 
 jest.setTimeout(2000);
 
@@ -50,7 +50,7 @@ describe("[p-01] GET /posts", () => {
 
     test("[pit-01-2] 성공적인 일기 조회 요청", async () => {
         // 사용자가 일기를 3개 등록한 상황을 가정한다.
-        await agent.post("/posts").send({ content: "일기 1" });
+        const post = await agent.post("/posts").send({ content: "일기 1" });
         await agent.post("/posts").send({ content: "일기 2" });
         await agent.post("/posts").send({ content: "일기 3" });
         
@@ -59,8 +59,81 @@ describe("[p-01] GET /posts", () => {
         expect(response.status).toBe(200);
         expect(response.body.diaries.length).toBe(3);
         expect(response.body.diaries[0]).toEqual({
-            id: expect.any(Number),
-            content: expect.any(String),
+            id: post.body.postId,
+            content: "일기 1",
+            writeDate: expect.any(String),
+            writeTime: expect.any(String),
+            emotions: expect.any(Array),
+            positiveScore: expect.any(Number),
+            negativeScore: expect.any(Number),
+        });
+    });
+});
+
+// [p-02] GET /posts/:postId
+describe("[p-02] GET /posts/:postId", () => {
+
+    const agent = request.agent(app);
+    const agent2 = request.agent(app);
+
+    // 모든 테스트 시작 전: 회원 가입
+    beforeAll(async () => {
+        await request(app).post("/users").send(joinUserInfo);
+        await request(app).post("/users").send(newJoinUserInfo);
+    });
+
+    // 각 테스트 시작 전: 로그인
+    beforeEach(async () => {
+        await agent.post("/users/login").send(loginUserInfo);
+        await agent2.post("/users/login").send(loginNewUserInfo);
+    });
+
+    // 각 테스트 종료 후: 로그아웃
+    afterEach(async () => {
+        await agent.post("/users/logout");
+        await agent2.post("/users/logout");
+    });
+
+    // 모든 테스트 종료 후: 회원 탈퇴
+    afterAll(async () => {
+        await agent.post("/users/login").send(loginUserInfo);
+        await agent.delete("/users").send({ confirmMessage: "회원 탈퇴를 희망합니다." });
+        await agent2.post("/users/login").send(loginNewUserInfo);
+        await agent2.delete("/users").send({ confirmMessage: "회원 탈퇴를 희망합니다." });
+    });
+
+    test("[pit-02-1] 로그인되지 않은 상태에서 일기 조회 요청", async () => {
+        const response = await request(app).get("/posts/1");
+
+        expect(response.status).toBe(403);
+        expect(response.text).toBe("로그인이 필요합니다.");
+    });
+    
+    test("[pit-02-2] 다른 사용자의 일기 조회 요청", async () => {
+        // 어떤 사용자가 일기 작성
+        const post = await agent.post("/posts").send({ content: "일기" });
+        // 다른 사용자가 일기 조회
+        const response = await agent2.get(`/posts/${post.body.postId}`);
+
+        expect(response.status).toBe(403);
+        expect(response.text).toBe("접근 권한이 없습니다.");
+    });
+
+    test("[pit-02-3] 존재하지 않는 ID를 가진 일기 조회 요청", async () => {
+        const response = await agent.get("/posts/-1");
+
+        expect(response.status).toBe(404);
+        expect(response.text).toBe("[id: -1] 일기가 존재하지 않습니다.");
+    });
+
+    test("[pit-02-4] 성공적인 일기 조회 요청", async () => {
+        const post = await agent.post("/posts").send({ content: "일기" });
+        const response = await agent.get(`/posts/${post.body.postId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body.diary).toEqual({
+            id: post.body.postId,
+            content: "일기",
             writeDate: expect.any(String),
             writeTime: expect.any(String),
             emotions: expect.any(Array),
