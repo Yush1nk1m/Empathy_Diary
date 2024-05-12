78dbde73c53c4f6d54e59b9e2748e2da4a1ed0bc
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-12 14:22:43 +0900

    Test: [p-06] 통합 테스트 작성

--- backend/routes/post3.test.js
+++ backend/routes/post3.test.js
@@ -77,3 +77,85 @@ describe("[p-05] DELETE /posts/:postId", () => {
         expect(response.text).toBe("일기가 삭제되었습니다.");
     });
 });
+
+// [p-06] GET /posts/period?[startDate]&[endDate]
+describe("[p-06] GET /posts/period?[startDate]&[endDate]", () => {
+
+    const agent = request.agent(app);
+    const yesterday = new Date(new Date().setHours(-24, 0, 0, 0));
+    const yesterdayString = `${yesterday.getFullYear()}-${yesterday.getMonth() + 1}-${yesterday.getDate()}`
+    const today = new Date();
+    const todayString = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+    const tomorrow = new Date(new Date().setHours(24, 0, 0, 0));
+    const tomorrowString = `${tomorrow.getFullYear()}-${tomorrow.getMonth() + 1}-${tomorrow.getDate()}`
+
+    // 모든 테스트 시작 전: 회원 가입
+    beforeAll(async () => {
+        await request(app).post("/users").send(joinUserInfo);
+    });
+
+    // 각 테스트 시작 전: 로그인
+    beforeEach(async () => {
+        await agent.post("/users/login").send(loginUserInfo);
+    });
+
+    // 각 테스트 종료 후: 로그아웃
+    afterEach(async () => {
+        await agent.post("/users/logout");
+    });
+
+    // 모든 테스트 종료 후: 회원 탈퇴
+    afterAll(async () => {
+        await agent.post("/users/login").send(loginUserInfo);
+        await agent.delete("/users").send({ confirmMessage: "회원 탈퇴를 희망합니다." });
+    });
+
+    test("[pit-06-1] 로그인되지 않은 상태에서 일기 조회 요청", async () => {
+        const response = await request(app).get(`/posts/period?startDate=${todayString}&endDate=${todayString}`);
+
+        expect(response.status).toBe(403);
+        expect(response.text).toBe("로그인이 필요합니다.");
+    });
+
+    test("[pit-06-2] 충분한 쿼리 파라미터 없이 일기 조회 요청", async () => {
+        const response = await agent.get("/posts/period");
+
+        expect(response.status).toBe(400);
+        expect(response.text).toBe("충분한 쿼리 파라미터가 제공되지 않았습니다.");
+    });
+
+    test("[pit-06-3] 유효하지 않은 쿼리 파라미터 값으로 일기 조회 요청", async () => {
+        const response = await agent.get("/posts/period?startDate=invalid&endDate=invalid");
+
+        expect(response.status).toBe(400);
+        expect(response.text).toBe("쿼리 파라미터의 값이 유효하지 않습니다.");
+    });
+
+    test("[pit-06-4] 성공적인 일기 조회 요청", async () => {
+        const posts = [];
+        posts.push(await agent.post("/posts").send({ content: "일기 1" }));
+        posts.push(await agent.post("/posts").send({ content: "일기 2" }));
+        posts.push(await agent.post("/posts").send({ content: "일기 3" }));
+
+        const yesterdayResponse = await agent.get(`/posts/period?startDate=${yesterdayString}&endDate=${yesterdayString}`);
+        const todayResponse = await agent.get(`/posts/period?startDate=${todayString}&endDate=${todayString}`);
+        const tomorrowResponse = await agent.get(`/posts/period?startDate=${tomorrowString}&endDate=${tomorrowString}`);
+
+        expect(yesterdayResponse.status).toBe(200);
+        expect(todayResponse.status).toBe(200);
+        expect(tomorrowResponse.status).toBe(200);
+
+        expect(yesterdayResponse.body.diaries).toEqual([]);
+        expect(tomorrowResponse.body.diaries).toEqual([]);
+        expect(todayResponse.body.diaries.length).toBe(3);
+        expect(todayResponse.body.diaries[0]).toEqual({
+            id: posts[0].body.postId,
+            content: "일기 1",
+            writeDate: expect.any(String),
+            writeTime: expect.any(String),
+            emotions: expect.any(Array),
+            positiveScore: expect.any(Number),
+            negativeScore: expect.any(Number),
+        });
+    });
+});
