9eb7c41c4cc4b9fb1c29166b2afabc1559d0f454
Yush1nk1m <kys010306@sogang.ac.kr>
2024-05-22 19:49:33 +0900

    Fix: [p-04] 오류 수정

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -284,12 +284,12 @@ exports.modifyDiaryContent = async (req, res, next) => {
         });
 
         // chatGPT API를 통해 일기 내용을 분석하고 감정, 감성 정보를 데이터베이스에 등록한다.
-        let LLMResponse = await analysisDiary(content);
+        let LLMResponse = await analysisDiary(newContent);
         // 환각 현상으로 올바른 데이터가 응답되지 않을 경우 다시 요청을 발생시킨다.
         const retryCount = 0;
         while (LLMResponse.emotions === undefined || LLMResponse.positiveScore === undefined || LLMResponse.negativeScore === undefined) {
             if (retryCount >= 3) throw new Error("반복적인 시도에도 환각 현상이 해결되지 않아 서버 에러를 발생시킵니다.");
-            LLMResponse = await analysisDiary(content);킵
+            LLMResponse = await analysisDiary(newContent);
             retryCount++;
         }
 
